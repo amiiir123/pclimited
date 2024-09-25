5b7ee4d27b3ee23236480233b8bdb731c14eb3cd
amiiir123 <itsazhary@gmail.com>
2024-09-25 18:32:20 +0100

    mopdification speed

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -175,7 +175,7 @@ Partners carousel
         $('#preloader').fadeOut('normal', function() {
             $(this).remove(); 
         });
-    }, 1500); 
+    }, 2000); 
 });
 
 /*------------------------------------
